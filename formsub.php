5118ebe40f734dc1e7f2f3bc489fbb2b7882f32a
Nick Givney <ngivney@hotmail.com>
2015-04-14 16:55:21 -0600

    Create formsub.php

---
+++ formsub.php
@@ -0,0 +1,16 @@
+<?php
+$URLhold = $_SERVER['HTTP_REFERER'];
+if ($_POST && $URLhold == "http://ngivney.comyr.com/github/jsphpinputmask/index.html" || $URLhold == "http://ngivney.comyr.com/github/jsphpinputmask/"){
+}
+else{
+header( 'Location: filloutform.html' ) ;
+}
+$_SESSION['wphone1'] = $_POST['wphone1'] ;
+$wphone1 = $_SESSION['wphone1'];
+$_SESSION['wphone2'] = $_POST['wphone2'] ;
+$wphone2 = $_SESSION['wphone2'];
+$_SESSION['wphone3'] = $_POST['wphone3'] ;
+$wphone3 = $_SESSION['wphone3'];
+$wphone = $wphone1 . "-" . $wphone2 . "-" . $wphone3;
+echo "You entered: $wphone";
+?>
